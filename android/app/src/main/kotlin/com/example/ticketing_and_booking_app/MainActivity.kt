f9f078f1190a5a891cd7646969d2576ee282741f
RutananaArnold <arnoldrutanana@gmail.com>
2022-05-28 18:17:55 +0300

    calender UI changes

---
+++ android/app/src/main/kotlin/com/example/ticketing_and_booking_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ticketing_and_booking_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/calenderUI.dart
@@ -0,0 +1,84 @@
+import 'package:booking_calendar/booking_calendar.dart';
+import 'package:flutter/material.dart';
+
+class CalenderUI extends StatefulWidget {
+  CalenderUI({Key? key}) : super(key: key);
+
+  @override
+  State<CalenderUI> createState() => _CalenderUIState();
+}
+
+class _CalenderUIState extends State<CalenderUI> {
+  final now = DateTime.now();
+  late BookingService doctorBooking;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    doctorBooking = BookingService(
+      serviceName: "Doctor/s schedule",
+      serviceDuration: 15,
+      bookingStart: DateTime(now.year, now.month, now.day, 8, 0),
+      bookingEnd: DateTime(now.year, now.month, now.day, 17, 0),
+    );
+  }
+
+  Stream<dynamic>? getBookingStreamMock(
+      {required DateTime end, required DateTime start}) {
+    return Stream.value([]);
+  }
+
+  Future<dynamic> uploadBookingMock(
+      {required BookingService newBooking}) async {
+    await Future.delayed(const Duration(seconds: 1));
+    converted.add(DateTimeRange(
+        start: newBooking.bookingStart, end: newBooking.bookingEnd));
+    print('${newBooking.toJson()} has been uploaded');
+  }
+
+  List<DateTimeRange> converted = [];
+
+  List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
+    ///here you can parse the streamresult and convert to [List<DateTimeRange>]
+    DateTime first = now;
+    DateTime second = now.add(const Duration(minutes: 55));
+    DateTime third = now.subtract(const Duration(minutes: 240));
+    DateTime fourth = now.subtract(const Duration(minutes: 500));
+    converted.add(
+        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
+    converted.add(DateTimeRange(
+        start: second, end: second.add(const Duration(minutes: 23))));
+    converted.add(DateTimeRange(
+        start: third, end: third.add(const Duration(minutes: 15))));
+    converted.add(DateTimeRange(
+        start: fourth, end: fourth.add(const Duration(minutes: 50))));
+    return converted;
+  }
+
+  List<DateTimeRange> pauseSlots = [
+    DateTimeRange(
+        start: DateTime.now().add(const Duration(minutes: 5)),
+        end: DateTime.now().add(const Duration(minutes: 60)))
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calender"),
+      ),
+      body: Center(
+        child: BookingCalendar(
+          bookingService: doctorBooking,
+          convertStreamResultToDateTimeRanges: convertStreamResultMock,
+          getBookingStream: getBookingStreamMock,
+          uploadBooking: uploadBookingMock,
+          pauseSlots: pauseSlots,
+          pauseSlotText: 'LUNCH',
+          hideBreakTime: false,
+        ),
+      ),
+    );
+  }
+}
